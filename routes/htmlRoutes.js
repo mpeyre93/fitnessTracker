aef1138b7935df0f2a5865795ecb01e63d116abd
Marlee Eyre <marlee.eyre@gmail.com>
2021-01-26 21:43:09 -0500

    comments added to html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,17 @@ const path = require("path");
 
 module.exports = (app) => {
     app.get("/", (req, res) => {
+        //renders main page
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     app.get("/exercise", (req, res) => {
+        //renders exercise page
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
     app.get("/stats", (req, res) => {
+        //renders stats page
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
 }
